7f5970a62ca6d1fc8600f0a0d71b1d33c60bedb1
Pea <pea@misfist.com>
2018-01-03 21:21:51 -0500

    Corrected parent style path

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -8,7 +8,7 @@
 add_action( 'wp_enqueue_scripts', 'learning_theme_enqueue_styles' );
 
 function learning_theme_enqueue_styles() {
-    wp_enqueue_style( 'wordstar-style', trailingslashit( get_template_directory_uri() ) . '/style.css' );
+    wp_enqueue_style( 'wordstar-style', trailingslashit( get_template_directory_uri() ) . 'style.css' );
     wp_enqueue_style( 'learning-style',
         trailingslashit( get_stylesheet_directory_uri() ) . 'style.css',
         array( 'wordstar-style' )
